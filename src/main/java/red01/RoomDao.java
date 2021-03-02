62fc92aa6cb4ca1bc7c7d46668fb2277a289434b
Tommaso Giromini <tommaso.giromini@gmail.com>
2021-03-02 10:13:06 +0100

    RoomDAo

---
+++ src/main/java/red01/RoomDao.java
@@ -0,0 +1,73 @@
+package red01;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class RoomDao {
+    static private final Logger log = LoggerFactory.getLogger(RoomDao.class);
+
+    public List<Room> readAll() {
+        EntityManager em = null;
+
+        try {
+            em = JpaUtil.createEntityManager();
+            String jpql = "SELECT r FROM Rooms r";
+            return em.createQuery(jpql, Room.class).getResultList();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Optional<Room> read(int id) {
+        EntityManager em = null;
+
+        try {
+            em = JpaUtil.createEntityManager();
+            return Optional.ofNullable(em.find(Room.class, id));
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public boolean create(Room room) {
+        log.trace("enter");
+        EntityManager em = null;
+        EntityTransaction tx = null;
+
+        try {
+            em = JpaUtil.createEntityManager();
+            tx = em.getTransaction();
+            tx.begin();
+            em.persist(room);
+            tx.commit();
+            return true;
+        } catch (Exception ex) {
+            log.warn("Can't persist entity", ex);
+            try {
+                if (tx != null && tx.isActive()) {
+                    tx.rollback();
+                }
+            } catch (Exception e) {
+                log.warn("Can't rollback transaction", e);
+            }
+
+            return false;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+}
