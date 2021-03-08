4357c02ee251f82c0b72f8ad10100a356722abf4
Tommaso Giromini <tommaso.giromini@gmail.com>
2021-03-08 10:01:38 +0100

    hash code and equals in slotID

--- src/main/java/red01/SlotId.java
+++ src/main/java/red01/SlotId.java
@@ -25,4 +25,29 @@ public class SlotId implements Serializable {
 		this.sid = sid;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + sid;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SlotId other = (SlotId) obj;
+		if (id != other.id)
+			return false;
+		if (sid != other.sid)
+			return false;
+		return true;
+	}
+
 }
